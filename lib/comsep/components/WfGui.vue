6ea7ef453d9b671737a03451b5d8d1e704927e86
vit <shiegin@gmail.com>
2019-10-11 12:42:49 +0300

    m

--- lib/comsep/components/WfGui.vue
+++ lib/comsep/components/WfGui.vue
@@ -2,9 +2,21 @@
     <div style="border: thin solid green;">
         <h1>Workflow GUI</h1>
         <div v-for="(qa, ind) in query_answer" v-if="qa && qa.name" :key="ind">
-            <div v-if="qa.name=='_what_can_i_do'">
-                !{{qa.result}}!
+
+            <div v-if="qa.name=='_what_can_i_do'" style="border: thin solid red; padding: 10px;">
+                <h3>What can I do now</h3>
+                <ul>
+                    <li v-for="[k,v] in Object.entries(qa.result.events)" :key="k">
+                        <span v-if="v.available">
+                            <nuxt-link :to="'author/new?event='+k">{{k}}</nuxt-link>
+                        </span>
+                        <span v-else>
+                            <strike>{{k}}</strike>
+                        </span>
+                    </li>
+                </ul>
             </div>
+
         </div>
         
         <!--p>wf_id: {{wf_id}}</p>
